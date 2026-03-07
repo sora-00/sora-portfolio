b6b220fe73e4ee64ce6415d84ff775fcea748e2f
sora-00 <sora.umiushi@gmail.com>
2026-03-07 22:34:04 +0900

    feat;猫型ステッカーとpopupダイアログ

--- src/app/ui/container/top.tsx
+++ src/app/ui/container/top.tsx
@@ -1,7 +1,18 @@
+"use client"
+import { Sticker } from "../common/sticker";
+
 export const Top = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Top</h1>
+    <div className="flex flex-col pl-10 pt-10 items-center gap-6">
+      <h1 className="text-4xl font-bold">Sora&apos;s Portfolio</h1>
+      <Sticker
+        title="自己紹介"
+        imageSrc="/sticker-box.png"
+        imageAlt="自己紹介"
+      >
+        自己紹介
+      </Sticker>
     </div>
   )
 }
+
