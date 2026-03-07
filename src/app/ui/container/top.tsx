54365cf16bd6c31b30f828a9d8f3e1869229f4df
sora-00 <sora.umiushi@gmail.com>
2026-03-07 23:52:19 +0900

    fix:画像の名前変更,title変更

--- src/app/ui/container/top.tsx
+++ src/app/ui/container/top.tsx
@@ -9,14 +9,14 @@ type StickerConfig = {
 }
 
 const STICKERS: StickerConfig[] = [
-  { id: 1, title: "紹介1", content: "ここに1の内容を書く" },
-  { id: 2, title: "紹介2", content: "ここに2の内容を書く" },
-  { id: 3, title: "紹介3", content: "ここに3の内容を書く" },
-  { id: 4, title: "紹介4", content: "ここに4の内容を書く" },
-  { id: 5, title: "紹介5", content: "ここに5の内容を書く" },
-  { id: 6, title: "紹介6", content: "ここに6の内容を書く" },
-  { id: 7, title: "紹介7", content: "ここに7の内容を書く" },
-  { id: 8, title: "紹介8", content: "ここに8の内容を書く" },
+  { id: 1, title: "自己紹介", content: "ここに1の内容を書く" },
+  { id: 2, title: "プログラミング歴", content: "ここに2の内容を書く" },
+  { id: 3, title: "技術スタック", content: "ここに3の内容を書く" },
+  { id: 4, title: "各種リンク", content: "ここに4の内容を書く" },
+  { id: 5, title: "制作物1", content: "ここに5の内容を書く" },
+  { id: 6, title: "制作物2", content: "ここに6の内容を書く" },
+  { id: 7, title: "記事", content: "ここに7の内容を書く" },
+  { id: 8, title: "my love", content: "ここに8の内容を書く" },
 ];
 
 export const Top = () => {
